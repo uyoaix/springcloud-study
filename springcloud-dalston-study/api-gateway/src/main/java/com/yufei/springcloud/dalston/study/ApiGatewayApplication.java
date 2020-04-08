a1715d3eb87c11ea2529b23aa87a96a35ee967d1
uyoaix <zi_mengling@yeah.net>
2020-04-09 02:58:02 +0800

    增加过滤器，过滤请求

--- springcloud-dalston-study/api-gateway/src/main/java/com/yufei/springcloud/dalston/study/ApiGatewayApplication.java
+++ springcloud-dalston-study/api-gateway/src/main/java/com/yufei/springcloud/dalston/study/ApiGatewayApplication.java
@@ -1,14 +1,22 @@
 package com.yufei.springcloud.dalston.study;
 
+import com.yufei.springcloud.dalston.study.filter.TokenFilter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.Bean;
 
 @EnableZuulProxy
 @SpringBootApplication
 public class ApiGatewayApplication {
 
+    @Bean
+    public TokenFilter tokenFilter(){
+        return new TokenFilter();
+    }
+
     public static void main(String[] args) {
         SpringApplication.run(ApiGatewayApplication.class, args);
     }
+
 }
