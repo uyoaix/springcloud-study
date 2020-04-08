c609610a15b065668f32903bb3fd6d63895b6ab9
uyoaix <zi_mengling@yeah.net>
2020-04-09 04:37:13 +0800

    trace-a traceb整合logstash

--- springcloud-dalston-study/trace-a/src/main/java/com/yufei/springcloud/dalston/study/TraceAApplication.java
+++ springcloud-dalston-study/trace-a/src/main/java/com/yufei/springcloud/dalston/study/TraceAApplication.java
@@ -17,6 +17,7 @@ import org.springframework.web.bind.annotation.RestController;
 @SpringBootApplication
 public class TraceAApplication {
 
+
     @Autowired
     private TraceClient traceClient;
 
--- springcloud-dalston-study/trace-b/src/main/java/com/yufei/springcloud/dalston/study/TraceBApplication.java
+++ springcloud-dalston-study/trace-b/src/main/java/com/yufei/springcloud/dalston/study/TraceBApplication.java
@@ -1,23 +1,29 @@
 package com.yufei.springcloud.dalston.study;
 
-import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@Slf4j
+import javax.servlet.http.HttpServletRequest;
+
 @RestController
 @EnableDiscoveryClient
 @SpringBootApplication
 public class TraceBApplication {
 
+    private final Logger logger = LoggerFactory.getLogger(getClass());
+
     @GetMapping("/trace-b")
-    public String trace(){
-        log.info("===<call trace-b>===");
+    public String trace(HttpServletRequest request){
+        logger.info("===<call trace-b, TraceId={}, SpanId={}>===",
+                request.getHeader("X-B3-TraceId"), request.getHeader("X-B3-SpanId"));
         return "trace-b";
     }
+
 
     public static void main(String[] args) {
         SpringApplication.run(TraceBApplication.class, args);
