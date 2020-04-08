72c6aff5f1df872b5b67ca11cad11ef9cceff718
uyoaix <zi_mengling@yeah.net>
2020-04-09 00:08:54 +0800

    Spring Cloud Config 客户端OC

---
+++ springcloud-dalston-study/config-client/src/main/java/com/yufei/springcloud/dalston/study/ConfigClientApplication.java
@@ -0,0 +1,13 @@
+package com.yufei.springcloud.dalston.study;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConfigClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigClientApplication.class, args);
+    }
+
+}
