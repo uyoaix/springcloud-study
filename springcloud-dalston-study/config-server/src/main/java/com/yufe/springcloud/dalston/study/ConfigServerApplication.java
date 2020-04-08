30327d16ae0caecd19bdb8836abea756fe56f209
uyoaix <zi_mengling@yeah.net>
2020-04-09 00:17:40 +0800

    Spring Cloud Config 服务端开启服务注册，注册到eureka，客户端就能以服务的方式进行访问

--- springcloud-dalston-study/config-server/src/main/java/com/yufe/springcloud/dalston/study/ConfigServerApplication.java
+++ springcloud-dalston-study/config-server/src/main/java/com/yufe/springcloud/dalston/study/ConfigServerApplication.java
@@ -2,10 +2,12 @@ package com.yufe.springcloud.dalston.study;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.config.server.EnableConfigServer;
 
-@SpringBootApplication
+@EnableDiscoveryClient
 @EnableConfigServer
+@SpringBootApplication
 public class ConfigServerApplication {
 
     public static void main(String[] args) {
